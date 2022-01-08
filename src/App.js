89a9dee6bc0f1e04088af0f12cbf15e97af0bb63
Matheus-Bezerra <matheusbwzerra002@gmail.com>
2022-01-08 15:40:03 -0300

    App com Rotas

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React from 'react';
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Header from './components/Header/Header';
 import Home from './components/Home';
+import Movie from './components/Movie';
+import NotFound from './components/NotFound';
 
 import { GlobalStyle } from './GlobalStyle';
 
 const App = () => {
   return (
     <div className="App">
-      <Header />
-      <Home />
-      <GlobalStyle />
-      Start here.
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/:movieId" element={<Movie />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+        <GlobalStyle />
+      </Router>
     </div>
   );
 };
