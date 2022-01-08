6f4cf87957e9889d3d3ac1cbc58a190839affd16
Matheus-Bezerra <matheusbwzerra002@gmail.com>
2022-01-08 15:42:27 -0300

    Componente de pesquisa funcional

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,7 +1,43 @@
 import React, { useState, useEffect, useRef } from 'react';
 
-const SearchBar = () => {
-  return <div></div>;
+import searchIcon from '../../images/search-icon.svg';
+
+import { Wrapper, Content } from './searchBar.styles';
+
+const SearchBar = ({ setSearchTerm }) => {
+  const [state, setState] = useState('');
+  const initial = useRef(true);
+
+  useEffect(() => {
+    if (initial.current) {
+      initial.current = false;
+      return;
+    }
+    const timer = setTimeout(() => {
+      setSearchTerm(state);
+    }, 500);
+
+    return () => clearTimeout(timer);
+  }, [setSearchTerm, state]);
+  return (
+    <Wrapper>
+      <Content>
+        <img src={searchIcon} alt="search-icon" />
+        <input
+          type="text"
+          placeholder="Search Movie"
+          onChange={({ target }) => {
+            if (target.value.trim() === '') {
+              setState('');
+            } else {
+              setState(target.value);
+            }
+          }}
+          value={state}
+        />
+      </Content>
+    </Wrapper>
+  );
 };
 
 export default SearchBar;
