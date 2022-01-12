9825ae52b2591a2fce35672f84a63437deafd5b9
Matheus-Bezerra <matheusbwzerra002@gmail.com>
2022-01-12 20:40:24 -0300

    Adiconando estilos para modo dark

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -7,6 +7,7 @@ export const GlobalStyle = createGlobalStyle`
     --lightGrey: #eee;
     --medGrey: #353535;
     --darkGrey: #1c1c1c;
+    --darkNight: #2D333B;
     --fontSuperBig: 2.5rem;
     --fontBig: 1.5rem;
     --fontMed: 1.2rem;
@@ -21,6 +22,7 @@ export const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
     padding: 0;
+    transition: all .8s;
 
     h1 {
       font-size: 2rem;
@@ -37,5 +39,19 @@ export const GlobalStyle = createGlobalStyle`
       font-size: 1rem;
       color: var(--white);
     }
+
+    .theme {
+    color: #1c1c1c;
+    transition: all .6s;
+    }
   }
+
+  &.night {
+    background: #2D333B;
+  
+    .theme {
+      color: var(--white);
+    }
+}
+
 `;
